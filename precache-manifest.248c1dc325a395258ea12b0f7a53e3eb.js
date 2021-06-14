b6a7ebf24a2a543164de5352b0a96f72e5396f2c
MKrasnansk <MKrasnansk@users.noreply.github.com>
2021-06-14 06:31:47 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.248c1dc325a395258ea12b0f7a53e3eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab6f099297867010790ac334234f935",
+    "url": "https://codingly-io.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "aff0ccc033c5e581270f",
+    "url": "https://codingly-io.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "8bb4a361227fa37832c1",
+    "url": "https://codingly-io.github.io/task-management-frontend/static/js/2.22acbf19.chunk.js"
+  },
+  {
+    "revision": "aff0ccc033c5e581270f",
+    "url": "https://codingly-io.github.io/task-management-frontend/static/js/main.20a66990.chunk.js"
+  },
+  {
+    "revision": "96a103aa2b1a8a298f52",
+    "url": "https://codingly-io.github.io/task-management-frontend/static/js/runtime~main.b2e8edb7.js"
+  }
+]);
